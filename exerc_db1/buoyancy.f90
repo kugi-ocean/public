fd4777965b41c53e9a70c373a2a156c6d8eef417
Kei Sakamoto <sakamoto.kei.6f@kyoto-u.ac.jp>
2026-06-02 17:29:07 +0900

    Fix db/buoyancy.f90

--- exerc_db1/buoyancy.f90
+++ exerc_db1/buoyancy.f90
@@ -7,7 +7,7 @@ program main
   real(8),parameter :: dt_sec = 10.d0    !- t方向の刻み幅
   real(8)    :: x(0:1)                   !- 変数 x(0): z [m], x(1): w (= dz/dt) [m/s]
   real(8)    :: t_sec
-  integer(8) :: i, nm
+  integer(8) :: n, nm
 
   nm = int( tend_sec / dt_sec )                   !- 総ステップ数
 
@@ -21,13 +21,13 @@ program main
   write(10) x
 
   write(6,*) '                   t,                        z,                        w'
-  do i = 0, nm-1
+  do n = 0, nm-1
      x = x + dt_sec * f(x,t_sec)    !- x,fとも2要素。オイラー法
      t_sec = t_sec + dt_sec
-     if ( ( mod(i,(nm/10)) == 0 ).or.( i == nm-1 ) ) write(6,*) t_sec,x
-     write(1) x
+     if ( ( mod(n,(nm/10)) == 0 ).or.( n == nm-1 ) ) write(6,*) t_sec,x
+     write(10) x
   enddo
-  close(1)
+  close(10)
 
 contains
 
